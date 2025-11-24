fa965ba5c8316c5041ef3e476c14819d836b1380
TrainingContentTeam <jashley@lexipol.com>
2025-11-24 14:01:14 -0500

    Add files via upload

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TrainingContentTeam-TrainingContentTeam-FO-Planning-and-Code-Enforcement_L1-Activity/',
+  base: './', // Use relative paths for GitHub Pages
 });
